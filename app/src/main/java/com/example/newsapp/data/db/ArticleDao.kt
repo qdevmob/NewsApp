94c7e408f3571c39530692581ee5633c81996596
123 <https://github.com/qdevmob/mvvm2.git>
2023-09-13 18:16:04 +0300

    dao

---
+++ app/src/main/java/com/example/newsapp/data/db/ArticleDao.kt
@@ -0,0 +1,20 @@
+package com.example.newsapp.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.newsapp.models.Article
+
+interface ArticleDao {
+
+    @Query("SELECT * from articles" )
+    suspend fun getAllArticles():LiveData<List<Article>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(article: Article)
+
+    @Delete
+    suspend fun delete(article: Article)
+}
